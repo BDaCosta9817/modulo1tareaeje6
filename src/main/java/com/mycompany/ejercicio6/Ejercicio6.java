dbd5c9f22515712019f8bca851a8adcb4d12bc3a
UserCP <UserCP@DESKTOP-EVBJTP8>
2024-11-03 20:58:51 -0600

    Carga inicial modulo1 ejercicio6

---
+++ src/main/java/com/mycompany/ejercicio6/Ejercicio6.java
@@ -0,0 +1,15 @@
+//Blanca Da Costa Gomez Carcamo
+//202310010031
+//Programa Para Numeros pares
+package com.mycompany.ejercicio6;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("Números pares del 2 al 100:");
+
+        //ciclo
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
